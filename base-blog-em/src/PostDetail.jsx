7137381a5ca8d196fcec934b157d2ac6b356deae
이챙 <cycy8527@gmail.com>
2022-12-08 23:15:48 +0900

    Add: comments

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,6 @@
+import { useQuery } from "react-query";
+
+//댓글 가져오기
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +26,16 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const {data,isLoading, isError, error} = useQuery(['comments',post.id],() => fetchComments(post.id));
+
+  if(isLoading) return <div>로딩즁</div>
+  if(isError){
+    return (
+      <>
+        <h3>{error.toString()}</h3>
+      </>
+    )
+  }
 
   return (
     <>
